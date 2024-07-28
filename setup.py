eedda6ecc9042533f5fc0306b9a75a9c1b9679f8
saidsurucu <saidsrc@gmail.com>
2024-07-28 12:34:16 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="yokatlas-py",
-    version="0.2.3",
+    version="0.2.4",
     author="Said Sürücü",
     author_email="saidsrc@gmail.com",
     description="A python wrapper for YOKATLAS API",
